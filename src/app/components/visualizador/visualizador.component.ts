c307db2f39dcbd9055995eec6a2e739cd51cd98f
danisamaniego370 <danisamaniego370@gmail.com>
2024-11-20 16:25:04 +0000

    V1.01

--- src/app/components/visualizador/visualizador.component.ts
+++ src/app/components/visualizador/visualizador.component.ts
@@ -1,4 +1,3 @@
-// src/app/components/visualizador/visualizador.component.ts
 import { Component } from '@angular/core';
 import { EmailComponent } from '../entradas/email/email.component';
 import { PasswordComponent } from '../entradas/password/password.component';
@@ -7,24 +6,38 @@ import { TextoComponent } from '../entradas/texto/texto.component';
 @Component({
   selector: 'app-visualizador',
   standalone: true,
-  imports: [EmailComponent, PasswordComponent, TextoComponent], // Importa los componentes aquí
+  imports: [EmailComponent, PasswordComponent, TextoComponent], 
   templateUrl: './visualizador.component.html',
   styleUrls: ['./visualizador.component.css']
 })
 export class VisualizadorComponent {
+  // Variables temporales para el formulario
+  tempEmail: string = '';
+  tempPassword: string = '';
+  tempTexto: string = '';
+
+  // Variables que se actualizan al hacer clic en el botón
   email: string = '';
   password: string = '';
   texto: string = '';
 
+  // Métodos para capturar los cambios del formulario
   onEmailChange(value: string) {
-    this.email = value;
+    this.tempEmail = value;
   }
 
   onPasswordChange(value: string) {
-    this.password = value;
+    this.tempPassword = value;
   }
 
   onTextoChange(value: string) {
-    this.texto = value;
+    this.tempTexto = value;
+  }
+
+  // Método para actualizar los resultados al presionar el botón
+  subirResultados() {
+    this.email = this.tempEmail;
+    this.password = this.tempPassword;
+    this.texto = this.tempTexto;
   }
 }
