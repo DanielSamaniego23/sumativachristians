29cc5f9e484ac087ac7721908739881178b2e361
danisamaniego370 <danisamaniego370@gmail.com>
2024-11-20 04:24:28 +0000

    first commit

---
+++ src/app/components/botones/boton-modo-oscuro/boton-modo-oscuro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boton-modo-oscuro',
+  standalone: true,
+  imports: [],
+  templateUrl: './boton-modo-oscuro.component.html',
+  styleUrl: './boton-modo-oscuro.component.css'
+})
+export class BotonModoOscuroComponent {
+
+}
